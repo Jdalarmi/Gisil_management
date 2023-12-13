2fc41f96cf0c5e8bc21aacf51ea4a30ee6efc727
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-12-13 15:22:53 -0300

    create template from 'entradas'i need implent logic from backend

--- gisil/views.py
+++ gisil/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gisil/index.html')
+
+def entry_value(request):
+    return render(request, 'gisil/gisil_values.html')
